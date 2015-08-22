15ea482cefbad3e314543d10334950788bd4e3a4
kylestratis <kylestratis@users.noreply.github.com>
2015-08-22 17:16:09 -0400

    style changes to bubble sort implementation

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,16 +1,19 @@
 # Implementation of BubbleSort
 
-def bubbleSort(unsortedList):
-    for passNumber in range(len(unsortedList)):
-        for cell in range(len(unsortedList) - 1):
-            if unsortedList[cell] > unsortedList[cell + 1]:
-                unsortedList[cell], unsortedList[cell + 1] = unsortedList[cell + 1],unsortedList[cell]
-    return unsortedList
+def bubble_sort(unsorted_list):
+    for pass_number in range(len(unsorted_list)):
+        for cell in range(len(unsorted_list) - 1):
+            if unsorted_list[cell] > unsorted_list[cell + 1]:
+                unsorted_list[cell], unsorted_list[cell + 1] = unsorted_list[cell + 1],unsorted_list[cell]
+    return unsorted_list
 
 # Some testing
 list = [2, 1, 6, 3, 7, 10]
+list2 = [10, 1, 4, 5, 2, 6]
 python = ['p', 'y', 't', 'h', 'o', 'n']
 print(list)
-print(bubbleSort(list))
+print(bubble_sort(list))
 print(python)
-print(bubbleSort(python))
+print(bubble_sort(python))
+print(list2)
+print(bubble_sort(list2))
